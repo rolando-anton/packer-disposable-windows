a70d3c1db93f2d77e16179a5c962b9699cf79e21
Rolando Anton <rolando@anton.sh>
2020-10-08 15:55:34 +0000

    removed disable windows features

--- scripts/disable-windows-features.ps1
+++
@@ -1,3 +0,0 @@
-Disable-WindowsOptionalFeature -NoRestart -online -FeatureName Internet-Explorer-Optional-amd64
-Disable-WindowsOptionalFeature -NoRestart -online -FeatureName WindowsMediaPlayer
-Disable-WindowsOptionalFeature -NoRestart -online -FeatureName MediaPlayback
