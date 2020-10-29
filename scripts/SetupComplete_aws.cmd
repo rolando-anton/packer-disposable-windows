374dc9b05591716ba586381d2dfc8ed03a64b008
Rolando Anton <rolando@anton.sh>
2020-10-29 20:34:27 +0000

    various aws fixes

--- scripts/SetupComplete_aws.cmd
+++ scripts/SetupComplete_aws.cmd
@@ -1,4 +1,3 @@
-powershell -c "New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BgInfo' -Value 'C:\ProgramData\chocolatey\lib\bginfo\Tools\Bginfo.exe C:\ProgramData\chocolatey\lib\bginfo\Tools\bginfo.bgi /silent /timer:0 /nolicprompt' -PropertyType 'String' -force"
 sc.exe config wuauserv start=disabled
 sc.exe stop wuauserv
 powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
